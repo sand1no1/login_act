a41f5abefcc14d2b3c58d819fb71354be4174b83
sand1no1 <64820057+sand1no1@users.noreply.github.com>
2026-02-26 12:00:44 -0600

    fix: asignación de class activo dependiendo del title

--- scripts/header_footer.js
+++ scripts/header_footer.js
@@ -1,12 +1,16 @@
+const title = document.title;
 const header = document.getElementsByTagName("header")[0];
+
 header.className = "header";
+
 header.innerHTML = 
 `
     <div class="logo">Mi Sitio</div>
     <nav class="menu">
-        <a href="inicio.html" class="activo">Inicio</a>
-        <a href="perfil.html">Perfil</a>
-        <a href="servicios.html">Servicios</a>
+        <a href="inicio.html"${title === "Inicio" ? ` class="activo"` : ""}>Inicio</a>
+        <a href="perfil.html"${title === "Perfil" ? ` class="activo"` : ""}>Perfil</a>
+        <a href="servicios.html"${title === "Servicios" ? ` class="activo"` : ""}>Servicios</a>
+        <a href="pokemon.html"${title === "Pokemon" ? ` class="activo"` : ""}>Pokemon</a>
     </nav>
 `;
 
